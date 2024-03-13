25cfac70c519febc283dca990f0c48e7d28c97ec
whitebbit <66116064+whitebbit@users.noreply.github.com>
2024-03-13 10:35:08 +0300

    rework player movement

--- Assets/3. Scripts/Units/Player/PlayerMovement.cs
+++ Assets/3. Scripts/Units/Player/PlayerMovement.cs
@@ -14,12 +14,10 @@ namespace _3._Scripts.Units.Player
 
 
         public float SpeedMultiplier { get; set; }
-        private Rigidbody rb;
         private IMovable movable;
         private UnitAnimator animator;
         private void Awake()
         {
-            rb = GetComponent<Rigidbody>();
             animator = GetComponent<UnitAnimator>();
             movable = new InputMovable(new JoystickInput(joystick));
         }
@@ -50,17 +48,18 @@ namespace _3._Scripts.Units.Player
         {
             var lookRotation = Quaternion.LookRotation(direction);
             var speed = config.MoveSpeed * (1 + SpeedMultiplier / 100);
-            rb.transform.rotation =
-                Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * config.RotationSpeed);
-            rb.MovePosition(rb.position + transform.forward * direction.magnitude * speed * Time.deltaTime);
+            var t = transform;
+            var position = t.position;
+            var targetPosition = position + t.forward * direction.magnitude * speed * Time.deltaTime;
+            
+            transform.rotation =
+                Quaternion.Lerp(t.rotation, lookRotation, Time.deltaTime * config.RotationSpeed);
+            transform.position = Vector3.Lerp(position, targetPosition, speed * Time.deltaTime);
             animator.SetFloat("Speed", direction.magnitude);
         }
 
         private void Stop()
         {
-            if (rb != null)
-                rb.velocity = Vector3.zero;
-            
             animator.SetFloat("Speed", 0);
         }
     }
