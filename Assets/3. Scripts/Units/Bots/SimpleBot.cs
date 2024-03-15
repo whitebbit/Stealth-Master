6260b8131299cadcc1c7ed4527872737cb6c216e
whitebbit <66116064+whitebbit@users.noreply.github.com>
2024-03-15 14:46:25 +0300

    simple bot beta complete

--- Assets/3. Scripts/Units/Bots/SimpleBot.cs
+++ Assets/3. Scripts/Units/Bots/SimpleBot.cs
@@ -1,6 +1,9 @@
 using _3._Scripts.FSM.Base;
+using _3._Scripts.Marks;
+using _3._Scripts.Marks.Enums;
 using _3._Scripts.Units.Bots.States;
 using _3._Scripts.Units.Interfaces;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -10,24 +13,51 @@ namespace _3._Scripts.Units.Bots
     {
         [SerializeField] private UnitNavMeshAgent unitNavMeshAgent;
 
-        [Header("FSM")]
-        [SerializeField] private SimpleBotFSM simpleBotFsm;
+        [Header("FSM")] [SerializeField] private SimpleBotFSM simpleBotFsm;
 
+        [Header("Visual")] [SerializeField] private MarkSystem markSystem;
+        [SerializeField] private MeshRenderer meshRenderer;
         protected override void InitializeFSM()
         {
             simpleBotFsm.Initialize(Fsm, unitNavMeshAgent);
             Fsm.StateMachine.SetState(simpleBotFsm.GetState<PatrolState>());
+
+            SubscribeToStates();
         }
         
-
         protected override void OnDetectorFind(IWeaponVisitor visitor)
         {
             simpleBotFsm.SetCurrentVisitor(visitor);
             simpleBotFsm.SetCurrentVisitorPosition(visitor);
 
             if (Fsm.StateMachine.CurrentState == simpleBotFsm.GetState<AttackState>()) simpleBotFsm.Chasing = true;
-            if (Fsm.StateMachine.CurrentState == simpleBotFsm.GetState<SearchState>()) simpleBotFsm.Chasing  = false;
+            if (Fsm.StateMachine.CurrentState == simpleBotFsm.GetState<SearchState>()) simpleBotFsm.Chasing = false;
         }
-
+        
+        private void SubscribeToStates()
+        {
+            simpleBotFsm.GetState<ChaseState>().OnEnterAction += () => QuestionMarkState(true);
+            simpleBotFsm.GetState<ChaseState>().OnExitAction += () => QuestionMarkState(false);
+            
+            simpleBotFsm.GetState<SearchState>().OnEnterAction += () => QuestionMarkState(true);
+            simpleBotFsm.GetState<SearchState>().OnExitAction += () => QuestionMarkState(false);
+            
+            simpleBotFsm.GetState<AttackState>().OnEnterAction += () => OnAttack(true);
+            simpleBotFsm.GetState<AttackState>().OnExitAction += () => OnAttack(false);
+        }
+        
+        private void QuestionMarkState(bool state)
+        {
+            markSystem.SetMark(state ? MarkType.Question : MarkType.None);
+        }
+        private void OnAttack(bool state)
+        {
+            var color = state ? Color.red : Color.white;
+            var sprite = state ? MarkType.Explanation : MarkType.None;
+            
+            color.a = meshRenderer.materials[0].color.a;
+            markSystem.SetMark(sprite);
+            meshRenderer.material.DOColor(color, 0.25f);
+        }
     }
 }
